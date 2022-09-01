63de66759c0ee0c341175bebd169a43518bd02a6
nicole branco <hyaluna@gmail.com>
2022-09-01 20:55:51 -0300

    fix models

--- lib/models/episode.dart
+++ lib/models/episode.dart
@@ -1,17 +1,17 @@
 class Episode {
-  String? id;
-  String? season;
-  String? name;
-  int? duration;
-  int? number;
-  bool? watched;
+  String id;
+  String season;
+  String name;
+  int duration;
+  int number;
+  bool watched;
 
   Episode({
-    this.id,
-    this.number,
-    this.name,
-    this.duration,
-    this.season,
-    this.watched
+    this.id = '',
+    this.number = 0,
+    this.name = '',
+    this.duration = 0,
+    this.season = '',
+    this.watched = false
   });
 }
