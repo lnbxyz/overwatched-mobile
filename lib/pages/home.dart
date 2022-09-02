b379478d6d2b7f24ed6c935de6d56ad2612a580a
mihmilicio <milenamilicio@hotmail.com>
2022-09-01 21:16:01 -0300

    change home list type to series

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:overwatched/models/serie.dart';
 import 'package:overwatched/pages/series_detail.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,13 +10,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> _data = [];
+  List<Serie> _data = [];
 
   void _fetchData() {
     setState(() {
       _data = [
-        'test',
-        'test 2'
+        Serie(
+          id: "1",
+          name: 'Russian Doll',
+          coverUrl: "https://www.tvguide.com/a/img/catalog/provider/1/1/1-7650473727.jpg",
+          description: "Russian Doll is a 30 minute comedy-fantasy-mystery-science fiction starring Natasha Lyonne as Nadia Vulvokov, Charlie Barnett as Alan Zaveri and Greta Lee as Maxine.",
+          releaseYear: "2019",
+          endingYear: "Present",
+          genres: ["Fantasy", "Comedy"],
+          score: 7.8
+        )
       ];
     });
   }
@@ -24,30 +33,48 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     _fetchData();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Overwatched'),
-      ),
-      body: ListView.builder(
-        itemCount: _data.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            child: Card(
-              key: ValueKey(_data[index]),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(_data[index]),
-              ),
-            ),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => SeriesDetailPage(name: _data[index]),
-                ),
-              );
-            },
-          );
-        }
-      )
+        appBar: AppBar(
+          title: const Text('Overwatched'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+            child: ListView.builder(
+                itemCount: _data.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: SerieCard(_data[index])
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              SeriesDetailPage(serie: _data[index]),
+                        ),
+                      );
+                    },
+                  );
+                })
+        )
     );
   }
+}
+
+class SerieCard extends StatelessWidget {
+  const SerieCard(this.serie, {super.key});
+
+  final Serie serie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      key: ValueKey(serie.id),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(serie.name),
+      ),
+    );
+  }
+
 }
