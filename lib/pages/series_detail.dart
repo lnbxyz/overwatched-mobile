4600126398355cc3f11b52eb8be68e8a9ba1ac37
nicole branco <hyaluna@gmail.com>
2022-09-01 21:26:52 -0300

    add scrollview, image

--- lib/pages/series_detail.dart
+++ lib/pages/series_detail.dart
@@ -14,12 +14,28 @@ class _SeriesDetailPageState extends State<SeriesDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.serie.name),
+      appBar: AppBar(
+        title: Text(widget.serie.name),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ShaderMask(
+              blendMode: BlendMode.srcATop,
+              shaderCallback: (Rect bounds) {
+                return const LinearGradient(
+                  colors: [Color.fromARGB(0, 255, 255, 255), Colors.white],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: [0.5, 0.9]
+                ).createShader(bounds);
+              },
+              child: Image.network(widget.serie.coverUrl)
+            )
+          ],
         ),
-        body: Center(
-          child: Text(widget.serie.name),
-        )
+      )
     );
   }
 }
