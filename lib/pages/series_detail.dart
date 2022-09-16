2e66c07be7270b331a5f10c22c67a75a9e2c2416
nicole branco <hyaluna@gmail.com>
2022-09-16 19:39:35 -0300

    series detail

--- lib/pages/series_detail.dart
+++ lib/pages/series_detail.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:overwatched/components/season_list.dart';
+import 'package:overwatched/components/series_info_ROW.dart';
 import 'package:overwatched/models/serie.dart';
 
 class SeriesDetailPage extends StatefulWidget {
@@ -21,17 +25,62 @@ class _SeriesDetailPageState extends State<SeriesDetailPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            ShaderMask(
-              blendMode: BlendMode.srcATop,
-              shaderCallback: (Rect bounds) {
-                return const LinearGradient(
-                  colors: [Color.fromARGB(0, 255, 255, 255), Colors.white],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  stops: [0.5, 0.9]
-                ).createShader(bounds);
-              },
-              child: Image.network(widget.serie.coverUrl)
+            Stack(
+              alignment: Alignment.bottomLeft,
+              children: [
+                ShaderMask(
+                  blendMode: BlendMode.srcATop,
+                  shaderCallback: (Rect bounds) {
+                    return const LinearGradient(
+                      colors: [
+                        Color.fromARGB(0, 250, 250, 250),
+                        Color.fromARGB(255, 250, 250, 250)
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.5, 0.9]
+                    ).createShader(bounds);
+                  },
+                  child: Image.network(widget.serie.coverUrl)
+                ),
+                Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                        widget.serie.name,
+                        style: Theme.of(context).textTheme.headline2
+                    )
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SeriesInfoRow(
+                      icon: Icons.calendar_today_outlined,
+                      text: '${widget.serie.releaseYear} - ${widget.serie.endingYear.isNotEmpty ? widget.serie.endingYear : 'Presente' }'
+                  ),
+                  const SizedBox(height: 8),
+                  SeriesInfoRow(
+                      icon: Icons.theater_comedy_outlined,
+                      text: widget.serie.genres.join(', ')
+                  ),
+                  const SizedBox(height: 8),
+                  SeriesInfoRow(
+                      icon: Icons.grade_outlined,
+                      text: '${widget.serie.score.toStringAsFixed(1)}/10.0'
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    widget.serie.description,
+                    style: Theme.of(context).textTheme.bodyMedium
+                  ),
+                  const SizedBox(height: 32),
+                  SeasonList(serieId: widget.serie.id),
+                  const SizedBox(height: 200),
+                ],
+              ),
             )
           ],
         ),
