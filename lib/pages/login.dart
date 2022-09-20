5f365f0c28e0a6cf25de4273e9e2320a754cda59
mihmilicio <milenamilicio@hotmail.com>
2022-09-19 21:45:04 -0300

    create Login page

---
+++ lib/pages/login.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  void _onClickButtonPressed(BuildContext context) {
+    _login(context);
+  }
+
+  void _login(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const HomePage(),
+      ),
+    );
+  }
+
+  void _onClickRegister(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const HomePage(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Overwatched'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    child: Text("Login",
+                        style: Theme.of(context).textTheme.headline3)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Username',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Senha',
+                    ),
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextButton(
+                            onPressed: () => _onClickRegister(context),
+                            child: const Text("Criar conta")),
+                        OutlinedButton(
+                            onPressed: () => _onClickButtonPressed(context),
+                            child: const Text('Entrar')),
+                      ],
+                    ))
+              ],
+            )));
+  }
+}
